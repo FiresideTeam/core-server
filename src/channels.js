8710d302d703a75518966924d87e396a2a0febcc
Henry Baldwin <11enry.me@gmail.com>
2020-08-24 05:32:59 -0500

    tryna get that ip

--- src/channels.js
+++ src/channels.js
@@ -9,7 +9,7 @@ module.exports = function (app) {
     // On a new real-time connection, add it to the anonymous channel
     app.channel('anonymous').join(connection)
     const time = new Date().toLocaleTimeString()
-    console.log(`${time} -- ${connection} joined`)
+    console.log(`${time} -- ${connection.toString()} joined`)
 
 
 
