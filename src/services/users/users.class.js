74dd39741292b9bdd7f9afd4f3894ce1646515c7
Henry Baldwin <11enry.me@gmail.com>
2020-08-24 02:26:50 -0500

    spelling

--- src/services/users/users.class.js
+++ src/services/users/users.class.js
@@ -5,7 +5,7 @@ exports.Users = class Users extends Service {
   create(data, params) {
     //This is the data needed from client upon account creation
     const {id, first_name, last_name, username, email, password, rooms} = data;
-    //The complete user as given to sequelize, sequleize will add a createdAt and updatedAt property automatically.
+    //The complete user as given to sequelize, sequelize will add a createdAt and updatedAt property automatically.
     //Thanks sequelize :)
     const userData = {
       //assigned in uuid hook
@@ -15,10 +15,10 @@ exports.Users = class Users extends Service {
       username,
       email,
       password,
-      //array of UUIDV4 repping room ids, room ids should be generated in the client that initiates the chat and then will be added to the arrays of the other ccounts
+      //array of UUIDV4 representing room ids, room ids should be generated in the client that initiates the chat and then will be added to the arrays of the other ccounts
       rooms
     }
-    //calls creat method from "service", which i thought would use the sequilize create, actually I think does use sequelize (feathers-sequelize?) create,
+    //calls create method from "service", which i thought would use the sequelize create, actually I think does use sequelize (feathers-sequelize?) create,
     //however its unclear how or if it knows which model to use because feathers-sequelize has trash-ass docs,
     return super.create(userData, params);
   }
