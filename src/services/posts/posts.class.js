fd2ded9372f1be747b925fd367f7056691671784
Henry Baldwin <11enry.me@gmail.com>
2020-08-24 02:17:28 -0500

    cleaned up posts class

--- src/services/posts/posts.class.js
+++ src/services/posts/posts.class.js
@@ -1,22 +1,5 @@
 const { Service } = require('feathers-sequelize');
 
 exports.Posts = class Posts extends Service {
-  create(data, params) {
-    //This is the data needed from client upon post creation
-    const {id, first_name, last_name, username, email, password, rooms} = data;
-    //the complete user as will be stored in the database
-    const userData = {
-      //assigned in uuid hook
-      id,
-      first_name,
-      last_name,
-      username,
-      email,
-      password,
-      rooms
-    }
-    //calls creat method from "service", which i though would use the sequilize create, actually I think does use sequelize create,
-    //however its unclear how or if it knows which model to use because feathers-sequelize has trash-ass docs,
-    return super.create(userData, params);
-  }
+
 };
